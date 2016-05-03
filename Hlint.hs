80f331006225fc8a6adf948441aef3e54b456bef
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2016-05-03 19:26:38 -0400

    first commit on monad-ste as a standalone package (original development as part of hopper code base)

---
+++ Hlint.hs
@@ -0,0 +1,10 @@
+import "hint" HLint.HLint
+
+ignore "Reduce duplication"
+ignore "Redundant lambda"
+ignore "Use >=>"
+ignore "Use const"
+ignore "Avoid lambda"
+ignore "Redundant flip"
+ignore "Use let"
+ignore "Redundant $"
